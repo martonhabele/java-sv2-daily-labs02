4a13588dab0a199365c8ad0555f9a367ba62d3ff
Marton Andras Habele <marton.habele@outlook.com>
2021-10-19 15:18:00 +0200

    Bill and Jill added

--- src/main/java/day01/HumanMain.java
+++ src/main/java/day01/HumanMain.java
@@ -4,7 +4,11 @@ public class HumanMain {
     public static void main(String[] args) {
         Human human = new Human("Bill", 12);
 
+        Human human1 = new Human("Jill", 30);
+
         System.out.println(human.introduction());
+
+        System.out.println(human1.introduction());
     }
 
 }
